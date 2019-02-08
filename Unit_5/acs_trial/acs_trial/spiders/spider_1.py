e2b1e11fe499f38ade81a68d6374d6c1e7b07849
Begecky <greenrlg@gmail.com>
2019-02-08 11:48:21 -0700

    2/8/19 @ 11:48

--- Unit_5/acs_trial/acs_trial/spiders/spider_1.py
+++ Unit_5/acs_trial/acs_trial/spiders/spider_1.py
@@ -1,33 +1,13 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
 
 class ACSspider(scrapy.Spider):
-			name = 'ACS'
-			
-			def start_requests(self):
-						 urls = ['https://api.census.gov/data/2017/acs/acs1?getNAME=B04004_083E&for=county:*&in=state:01&key=db912d46f4c0fc8b306cc0ffe95dfbe707d9f1a3',
-												'https://api.census.gov/data/2017/acs/acs1?getNAME=B04004_083E&for=county:*&in=state:02&key=db912d46f4c0fc8b306cc0ffe95dfbe707d9f1a3',
-												'https://api.census.gov/data/2017/acs/acs1?getNAME=B04004_083E&for=county:*&in=state:03&key=db912d46f4c0fc8b306cc0ffe95dfbe707d9f1a3',
-												'https://api.census.gov/data/2017/acs/acs1?getNAME=B04004_083E&for=county:*&in=state:04&key=db912d46f4c0fc8b306cc0ffe95dfbe707d9f1a3',
-												'https://api.census.gov/data/2017/acs/acs1?getNAME=B04004_083E&for=county:*&in=state:05&key=db912d46f4c0fc8b306cc0ffe95dfbe707d9f1a3',
-												'https://api.census.gov/data/2017/acs/acs1?getNAME=B04004_083E&for=county:*&in=state:06&key=db912d46f4c0fc8b306cc0ffe95dfbe707d9f1a3',
-												'https://api.census.gov/data/2017/acs/acs1?getNAME=B04004_083E&for=county:*&in=state:07&key=db912d46f4c0fc8b306cc0ffe95dfbe707d9f1a3',
-												'https://api.census.gov/data/2017/acs/acs1?getNAME=B04004_083E&for=county:*&in=state:08&key=db912d46f4c0fc8b306cc0ffe95dfbe707d9f1a3',
-												'https://api.census.gov/data/2017/acs/acs1?getNAME=B04004_083E&for=county:*&in=state:09&key=db912d46f4c0fc8b306cc0ffe95dfbe707d9f1a3',
-												'https://api.census.gov/data/2017/acs/acs1?getNAME=B04004_083E&for=county:*&in=state:10&key=db912d46f4c0fc8b306cc0ffe95dfbe707d9f1a3',
-												]
-						 for url in urls:
-										yield scrapy.Request(url = url, callback = self.parse)
-																
-			def parse(self, response):
-							jsonresponse = json.loads(response.text)
-						        
-							item = MyItem()
-							item['PopCount'] = jsonresponse["B04004_083E"]
-							item['Name'] = jsonresponse['NAME']
-							item['State'] = jsonresponse['state']
-							item['County'] = jsonresponse['county']
-						        
-							return item
-										
-						
+
+    name = 'ACS'
+
+    def start_requests(self):
+        urls = ['https://api.census.gov/data/2015/acs/acs5?get=NAME,B04004_083E&for=county_subdivision:*&in=state:*&in=county*&key=db912d46f4c0fc8b306cc0ffe95dfbe707d9f1a3']
+        for url in urls:
+            yield scrapy.Request(url=url, callback = self.parse)
+        
+    def parse(self, response):
+        yield response
